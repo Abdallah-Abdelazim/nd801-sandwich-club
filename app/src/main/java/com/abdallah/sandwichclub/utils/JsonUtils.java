cb39216241875b66fc4645ab0ac8cbdc9290faf9
Abdallah Abdelazim <abdallah.abdelazim@hotmail.com>
2018-03-07 02:02:58 +0200

    Initial commit

---
+++ app/src/main/java/com/abdallah/sandwichclub/utils/JsonUtils.java
@@ -0,0 +1,10 @@
+package com.abdallah.sandwichclub.utils;
+
+import com.abdallah.sandwichclub.model.Sandwich;
+
+public class JsonUtils {
+
+    public static Sandwich parseSandwichJson(String json) {
+        return null;
+    }
+}
